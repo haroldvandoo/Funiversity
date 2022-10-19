5428180e976f92232274d58dda6c4d03ac9d7c70
Harold van Doorslaer <haroldvandoorslaer@hotmail.com>
2022-10-19 11:21:57 +0200

    first push

---
+++ src/test/java/com/example/funiversity/FuniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.funiversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuniversityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
